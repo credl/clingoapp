3fc3b91d450b685f7f612475917038e246a59dff
Christoph Redl <redl@kr.tuwien.ac.at>
2019-01-15 00:17:53 +0100

    Add project files.

---
+++ ClingoApp/ClingoApp/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ClingoApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+//            ThreadPool.QueueUserWorkItem(BackgroundThread);
+        }
+
+        /*
+        BackgroundThread()
+        {
+            System.Threading.Thread.Sleep(5000);
+            // When the download is complete, notify the UI
+            MonoTouch.Foundation.NSObject.BeginInvokeOnMainThread(delegate {
+                status.Text = "Download has been completed";
+            });
+        }
+        */
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ClingoApp/ClingoApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ClingoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        [DllImport("libDynLib.so", EntryPoint = "addGlobal")]
+        public static extern int add(int a, int b);
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            output.Text = "clicked: " + add(4, 5);
+
+        }
+        */
+
+        void btnStartSolver_Clicked(object sender, EventArgs args)
+        {
+            ASPSolver tp1 = new ASPSolver();
+            this.Navigation.PushAsync(tp1);
+
+        }
+    }
+}
